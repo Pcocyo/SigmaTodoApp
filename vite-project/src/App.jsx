f1be47f5ebfe2c85d4d6c4c6ed1508ebd1c19f06
Pcocyo <joshuaelson46@gmail.com>
2025-04-09 17:37:24 +0800

    (feat): ui- Yearview user Interface basic styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,15 +4,16 @@ import { predefinedCalendar } from './calender'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Row, Col, Container, Button } from 'react-bootstrap'
 import Navigation from './component/Navigation'
+import YearView from './component/YearView'
 const App = () => {
   return (
     <BrowserRouter>
-      <Container fluid className='border border-danger'>
+      <Container fluid className=''>
         <Row className="w-100" style={{maxHeight:'100vh'}}>
           <Navigation/>
-          <Col className=''>
+          <Col className='p-0 m-0' style={{backgroundColor:'#f9f9f9'}}>
             <Routes>
-
+              <Route path='/' element={<YearView/>}/>
             </Routes>
           </Col>
         </Row>
---
+++ vite-project/src/component/YearView.jsx
@@ -0,0 +1,60 @@
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Container,Row,Col } from 'react-bootstrap';
+import { predefinedCalendar } from '../calender';
+const YearView = () => {
+    const [selectedMonth, setSelectedMonth] = useState({ month: '', index: 0 });
+    const [calenderHover,setCalenderHover] = useState({})
+
+    function handleMouseEnterCalender(id){
+        setCalenderHover((prevState)=>({...prevState,[id]:true}))
+    }
+    function handleMouseLeaveCalender(id){
+        setCalenderHover((prevState)=>({...prevState,[id]:false}))
+    }
+    function monthBackgroundColor(id){
+        if (calenderHover[id] && id % 2 == 0){
+            return '#c8a1d6'
+        }
+        else if(calenderHover[id]){
+            return '#cfdedb'
+        } 
+    }
+    const navigate = useNavigate()
+    function handleSelectedMonth(month, monthInObjIndex) {
+        const newSelection = { month, index: monthInObjIndex };
+        setSelectedMonth(newSelection);
+        navigate('/week', { state: { ...newSelection } })
+    }
+    // border:`3px  solid  ${monthBackgroundColor(index)}`,
+    // backgroundColor:`${index%2==0? '#c8a1d6':'#cfdedb'}`,
+    return (
+        <Container fluid className=' h-100 d-flex align-items-center justify-content-center' style={{borderWidth:'2px'}}>
+            <Row className=' p-0 m-0 gap-2' style={{ height: '800px', width:'800px' }}>
+                {
+                    Object.keys(predefinedCalendar).map((keys, index) => {  
+                        return (
+                            <Col xs='12' sm='3' className='p-0 m-0 d-flex justify-content-center' 
+                            style={{ 
+                                cursor: 'pointer',
+                                color: '#030303',
+                                backgroundColor:`${calenderHover[index]?'transparent':index%2==0?"#c8a1d6":"#cfdedb"}`,
+                                border:`3px  solid  ${index%2==0?"#c8a1d6":"#cfdedb"}`,
+                                borderRadius:'10px'
+                            }} 
+                                key={index}
+                                onMouseEnter={()=>handleMouseEnterCalender(index)}
+                                onMouseLeave={()=>handleMouseLeaveCalender(index)}
+                                onClick={() => handleSelectedMonth(keys, index)}
+                            >
+                                <div className="fs-5">{keys}</div>
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+        </Container>
+    )
+}
+
+export default YearView
