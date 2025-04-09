fac7de06ad8a6811e7b6afaf2213bbb0a605f6d9
Pcocyo <joshuaelson46@gmail.com>
2025-04-09 08:59:04 +0800

    (feat): Basic react-router-dom setups, and defining predefinedCalender object

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import { predefinedCalendar } from './calender'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route></Route> */}
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
-
+// this is for a test
+// import MonthView from '../uiTest/MonthView'
+// import YearView from '../uiTest/YearView'
+// import WeekView from '../uiTest/WeekView'
 export default App
---
+++ vite-project/src/calender.js
@@ -0,0 +1,15 @@
+export const predefinedCalendar = {
+    January: Array(31).fill([]),
+    February: Array(28).fill([]), // Adjust leap year later dynamically, if needed
+    March: Array(31).fill([]),
+    April: Array(30).fill([]),
+    May: Array(31).fill([]),
+    June: Array(30).fill([]),
+    July: Array(31).fill([]),
+    August: Array(31).fill([]),
+    September: Array(30).fill([]),
+    October: Array(31).fill([]),
+    November: Array(30).fill([]),
+    December: Array(31).fill([])
+  };
+  
